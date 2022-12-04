af89e49c7e9370077ea603ba6602784fe8ca593f
Ian Edwards <ian.philip.edwards@gmail.com>
2022-12-04 18:58:58 +0000

    initial http

---
+++ http.py
@@ -0,0 +1,2 @@
+def get(url):
+    print(url)
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-import log
 import led
 import wlan
+import http
 from time import sleep
+from urequests import get
 
-log.info('system init...')
-log.info('wlan init...')
+print('system init...')
+print('wlan init...')
 (wlan_ssid, wlan_connected, wlan_status) = wlan.connect()
 if wlan_connected:
-    log.info(f'wlan connected')
+    print('wlan connected')
+    http.get('http://ip-api.com/json/')
 else:
-    log.error(f'wlan error: {wlan_status}')
+    print(f'wlan error: {wlan_status}')
 
 while True:
     led.toggle()
