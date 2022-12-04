74fc86b8ed83856bca15650a24119a550aa96706
Ian Edwards <ian.philip.edwards@gmail.com>
2022-12-04 18:26:32 +0000

    updated wlan timeout

--- wlan.py
+++ wlan.py
@@ -1,15 +1,15 @@
 import network
-from time import ticks_ms, sleep
+from time import ticks_ms, ticks_add, ticks_diff, sleep
 from ucollections import namedtuple
 from secret import NETWORK_SSID, NETWORK_PASSWORD
 from settings import WLAN_CONNECT_TIMEOUT, WLAN_CONNECT_POLL_INTERVAL
 
 def connect():
-    start = ticks_ms()
+    timeout = ticks_add(ticks_ms(), WLAN_CONNECT_TIMEOUT)
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(NETWORK_SSID, NETWORK_PASSWORD)
-    while not wlan.isconnected():
+    while not wlan.isconnected() and ticks_diff(timeout, ticks_ms()) > 0:
         sleep(WLAN_CONNECT_POLL_INTERVAL)
     Result = namedtuple('WlanConnectResult', 'ssid connected status')
     return Result(NETWORK_SSID, wlan.isconnected(), wlan.status()) # todo stringify status
