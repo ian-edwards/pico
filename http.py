d1c8f6307607421a2034a5f0fdd6511e0714960d
Ian Edwards <ian.philip.edwards@gmail.com>
2022-12-07 19:43:30 +0000

    partial url broken

--- http.py
+++ http.py
@@ -1,9 +1,7 @@
 import socket
 import url
 
-def get(uri):
-    #sock = socket.socket()
-    port = url.port(uri)
-    print(port)
-    addr = socket.getaddrinfo('google.com', port)[0][-1]
+def get(urlv):
+    (address, port) = url.info(urlv)
+    addr = socket.getaddrinfo(address, port)[0][-1]
     print(addr)
--- url.py
+++ url.py
@@ -1,15 +1,34 @@
-import re
+from ucollections import namedtuple
 
-__all__ = ['port']
+def info(url):
+    Result = namedtuple('UrlInfoResult', 'success address port')
+    (port_ok, port, port_length) = _port(url)
+    print(port_ok)
+    print(port)
+    print(port_length)
+    return Result(True, _address(url), port)
 
-regex = re.compile('^[^:]*')
+def _address(url):
+    return f'google.com'
 
-def port(url):
-    match = regex.match(url)
-    value = match.group(0)
-    if value == 'http':
+def _protocol(url):
+    Result = namedtuple('UrlProtocolResult', 'success value length')
+    for i in range(len(url)):
+        if (url[i] == ':'):
+            return Result(True, url[:i-1], i)
+    return Result(False, None, 0)
+
+def _port(url):
+    protocol = _protocol(url)
+    if protocol == 'http':
         return (True, 80)
-    elif value == 'https':
+    elif protocol == 'https':
         return 443
     else:
         return None
+    
+def _root(url):
+    return 'google'
+
+def _tld(url):
+    return 'com'
